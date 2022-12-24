0afff21e4810b8640e55a74e4296a0aa4bb47776
Erik Nagel <erik@reallyhuman.net>
2022-12-24 22:14:05 +0100

    MainWindow.xaml.cs um alte Kommentare bereinigt.

--- ZoomBoxDemo/MainWindow.xaml.cs
+++ ZoomBoxDemo/MainWindow.xaml.cs
@@ -7,18 +7,14 @@ namespace ZoomBoxDemo
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		//public TestVM TestContext { get; set; }
-
 		public MainWindow()
 		{
-			//this.TestContext = new TestVM();
 			InitializeComponent();
 			this.Loaded += MainWindow_Loaded;
 		}
 
 		void MainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
-			//this.TestContext.LoadData();
 		}
 	}
 }
